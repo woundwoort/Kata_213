7ba5c1347fda79ae10d0f401a836a06fec60fb28
woundwoort <woundwortt@yandex.ru>
2023-06-06 20:41:48 +0300

    Solved 2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,25 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getMessage());
+
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+
+        System.out.println("bean == bean 1 " + (bean1 == bean2));
+        System.out.println("bean2 == bean 3 " + (cat1 == cat2));
+
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,30 @@
+
+
+public class Cat {
+    private int id;
+
+    public Cat(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Cat() {
+    }
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
